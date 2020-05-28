7bc7f86af7f2bacdb1c4b0e78e8cdd3718d04ea5
Rory William <64885374+TheRoryWillAim@users.noreply.github.com>
2020-05-28 21:04:46 +0530

    Update todo_script.js

--- assests/js/todo_script.js
+++ assests/js/todo_script.js
@@ -5,3 +5,10 @@ $("li").click(function(){
     
 });
 
+//Click x to delete todo
+$("span").click(function(event){
+    $(this).parent().fadeOut(500,function(){
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
