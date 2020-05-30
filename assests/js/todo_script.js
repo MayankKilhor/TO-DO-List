f179eee9315ffc6316a86f3bc8eabc20f9c6da65
Rory William <64885374+TheRoryWillAim@users.noreply.github.com>
2020-05-30 19:25:46 +0530

    Update todo_script.js

--- assests/js/todo_script.js
+++ assests/js/todo_script.js
@@ -1,4 +1,3 @@
-
 //Check off specific todos by clicking
 $("li").click(function(){
     $(this).toggleClass("completed");
@@ -11,4 +10,14 @@ $("span").click(function(event){
         $(this).remove();
     });
     event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+    if(event.which === 13){
+        //grabbing new todo text from input
+        var TODOtext = $(this).val();
+        //create a new li and add to ul
+        $("ul").append("<li><span>X </span>" + TODOtext + "</li>");
+
+    }
 });
